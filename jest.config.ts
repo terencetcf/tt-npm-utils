fe0a38374b8f840b8a52957f01a60e6bfa3bee24
Terence Tai <terencetcf@hotmail.com>
2021-10-09 01:56:15 +0800

    perf(clean): project clean up
    - all structure should clean and lean.
    
    BREAKING CHANGES: Should ignore all previous version and use this version instead

--- jest.config.ts
+++ jest.config.ts
@@ -2,13 +2,9 @@ import type { Config } from '@jest/types';
 
 const config: Config.InitialOptions = {
   preset: 'ts-jest',
-  testEnvironment: 'node',
   testMatch: ['<rootDir>/test/**/*.test.ts'],
-  modulePathIgnorePatterns: ['<rootDir>/lib', '<rootDir>/src/package.json'],
-  collectCoverageFrom: [
-    '<rootDir>/src/**/*.ts',
-    '!<rootDir>/src/types/**/*.ts',
-  ],
+  modulePathIgnorePatterns: ['<rootDir>/lib'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
   globals: {
     'ts-jest': {
       diagnostics: false,
